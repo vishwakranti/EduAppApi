6bbe2907e72bfca9002c6ddf88ee584ca6286eb0
vishwakranti <vishwakrantisuryawanshi@gmail.com>
2023-07-24 16:09:01 +1200

    added insert sql for dummy data in to the students table

---
+++ Data/InsertData_Students.sql
@@ -0,0 +1,12 @@
+﻿USE [aspnet-EduAppApi]
+GO
+INSERT [dbo].[Students] ([Id], [FirstName], [MiddleName], [LastName], [Address], [Email], [PhoneNumber], [Gender], [DateofBirth]) VALUES (N'ee9fb96d-933e-4bb9-95ea-0af0917ddd6b', N'Bob', N'D', N'Keeith', N'567 abc road', N'bobkeef@hotmail.com', N'98765652', N'Male', N'25/12/1990')
+GO
+INSERT [dbo].[Students] ([Id], [FirstName], [MiddleName], [LastName], [Address], [Email], [PhoneNumber], [Gender], [DateofBirth]) VALUES (N'6b9d69f5-a5a6-4d10-82a7-3449f8ce9617', N'Dwayne', N'j', N'rock', N'Rocky street', N'djjohnson@hollywood.com', N'232312124', N'Male', N'15/12/1980')
+GO
+INSERT [dbo].[Students] ([Id], [FirstName], [MiddleName], [LastName], [Address], [Email], [PhoneNumber], [Gender], [DateofBirth]) VALUES (N'a4472422-9fa3-4e2a-935c-5c6f03c9b36e', N'Joe', N'H', N'Smith', N'123 xyz street', N'joesmith@gmail.com', N'12345678', N'Male', N'25/12/1985')
+GO
+INSERT [dbo].[Students] ([Id], [FirstName], [MiddleName], [LastName], [Address], [Email], [PhoneNumber], [Gender], [DateofBirth]) VALUES (N'041bfb3c-b678-4097-86f3-a7ec7362138d', N'Joan', N'K', N'Collins', N'897 cvv ave', N'joan@skynet.com', N'456987456', N'Female', N'25/12/991')
+GO
+INSERT [dbo].[Students] ([Id], [FirstName], [MiddleName], [LastName], [Address], [Email], [PhoneNumber], [Gender], [DateofBirth]) VALUES (N'22dde350-4c38-4fd3-b192-cbceb8d1ab39', N'Deidre', N'M', N'Joon', N'asdf river street', N'djoon@yahoo.com', N'12434124', N'Female', N'15/12/1995')
+GO
